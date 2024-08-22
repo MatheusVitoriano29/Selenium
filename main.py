c78ebf150f16f9b3a38e4d3d6a640d56ca914984
MatheusVitoriano29 <matheusvitorianooooj@gmail.com>
2024-08-22 17:57:55 -0300

    main.py

--- main.py
+++ main.py
@@ -10,12 +10,6 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
-# Chrome Options
-# https://peter.sh/experiments/chromium-command-line-switches/
-# Doc Selenium
-# https://selenium-python.readthedocs.io/locating-elements.html
-
-
 # Caminho para a raiz do projeto
 ROOT_FOLDER = Path(__file__).parent
 # Caminho para a pasta onde o chromedriver está
@@ -25,7 +19,6 @@ CHROME_DRIVER_PATH = ROOT_FOLDER / 'drivers' / 'chromedriver.exe'
 def make_chrome_browser(*options: str) -> webdriver.Chrome:
     chrome_options = webdriver.ChromeOptions()
 
-    # chrome_options.add_argument('--headless')
     if options is not None:
         for option in options:
             chrome_options.add_argument(option)
@@ -45,15 +38,11 @@ def make_chrome_browser(*options: str) -> webdriver.Chrome:
 if __name__ == '__main__':
     TIME_TO_WAIT = 15
 
-    # Example
-    # options = '--headless', '--disable-gpu',
     options = ()
     browser = make_chrome_browser(*options)
 
-    # Como antes
     browser.get('https://www.google.com/webhp')
 
-    # Espere para encontrar o input
     search_input = WebDriverWait(browser, TIME_TO_WAIT).until(
         EC.presence_of_element_located(
             (By.ID, 'APjFqb')
@@ -65,9 +54,5 @@ if __name__ == '__main__':
     
     search_input.send_keys(Keys.ENTER)
     sleep(TIME_TO_WAIT)
-
-
-    # Dorme por 10 segundos
-    # sleep(TIME_TO_WAIT)
 
     
